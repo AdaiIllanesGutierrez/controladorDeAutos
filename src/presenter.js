221b673cf71aa9014b59c1a9953e942f6dfbabe5
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-04-03 23:19:33 -0400

    crear y muestra una matriz con filas y columnas ingresadas por el usuario

--- src/presenter.js
+++ src/presenter.js
@@ -1,46 +1,42 @@
 //import Matriz from "./controladorDeAuto";
 
-// const first = document.querySelector("#primer-numero");
-// const second = document.querySelector("#segundo-numero");
-// const form = document.querySelector("#sumar-form");
+const f = document.querySelector("#primer-numero");
+const c = document.querySelector("#segundo-numero");
+const form = document.querySelector("#sumar-form");
 // const div = document.querySelector("#resultado-div");
 // Obtener la referencia al elemento div que contendrá la tabla
 var tablaDiv = document.getElementById("tabla-matriz");
 
-function Matriz(filas, columnas) {
-  var matriz = [];
-  for (var i = 0; i < filas; i++) {
-    matriz[i] = [];
-    for (var j = 0; j < columnas; j++) {
-      
-      matriz[i][j] = "0"; 
+
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let filas = Number.parseInt(f.value);
+  let columnas = Number.parseInt(c.value);
+  var miMatriz=Matriz(filas,columnas);
+  function Matriz(filas, columnas) {
+    var matriz = [];
+    for (var i = 0; i < filas; i++) {
+      matriz[i] = [];
+      for (var j = 0; j < columnas; j++) {
+        
+        matriz[i][j] = "0"; 
+      }
     }
+    return matriz;
   }
-  return matriz;
-}
-var miMatriz=Matriz(3,4);
-console.log(miMatriz);
+ 
+  var tabla = document.createElement("table");
 
-var tabla = document.createElement("table");
-var encabezado = tabla.createTHead().insertRow();
-
-for (var i = 0; i < miMatriz.length; i++) {
+  for (var i = 0; i < miMatriz.length; i++) {
   var fila = tabla.insertRow();
   for (var j = 0; j < miMatriz[i].length; j++) {
     var celda = fila.insertCell();
     celda.appendChild(document.createTextNode(miMatriz[i][j]));
   }
 }
-
-// Agregar la tabla al elemento div
 tablaDiv.appendChild(tabla);
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  // const firstNumber = Number.parseInt(first.value);
-  // const secondNumber = Number.parseInt(second.value);
-
-  // div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
- 
 });
