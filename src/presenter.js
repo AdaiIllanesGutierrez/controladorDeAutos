374d649ce14f9a2dc617bd0f7b00fe538d50a89c
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-04-03 23:06:08 -0400

    crear y muestra una matriz con datos estaticos

--- src/presenter.js
+++ src/presenter.js
@@ -4,6 +4,8 @@
 // const second = document.querySelector("#segundo-numero");
 // const form = document.querySelector("#sumar-form");
 // const div = document.querySelector("#resultado-div");
+// Obtener la referencia al elemento div que contendrá la tabla
+var tablaDiv = document.getElementById("tabla-matriz");
 
 function Matriz(filas, columnas) {
   var matriz = [];
@@ -18,6 +20,20 @@ function Matriz(filas, columnas) {
 }
 var miMatriz=Matriz(3,4);
 console.log(miMatriz);
+
+var tabla = document.createElement("table");
+var encabezado = tabla.createTHead().insertRow();
+
+for (var i = 0; i < miMatriz.length; i++) {
+  var fila = tabla.insertRow();
+  for (var j = 0; j < miMatriz[i].length; j++) {
+    var celda = fila.insertCell();
+    celda.appendChild(document.createTextNode(miMatriz[i][j]));
+  }
+}
+
+// Agregar la tabla al elemento div
+tablaDiv.appendChild(tabla);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
