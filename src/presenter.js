1e6f37b96cc83130c9770d6c86f98c9041c07ed5
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-04-03 22:55:10 -0400

    crear una matriz en js

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,30 @@
-import sumar from "./sumador";
+//import Matriz from "./controladorDeAuto";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+// const first = document.querySelector("#primer-numero");
+// const second = document.querySelector("#segundo-numero");
+// const form = document.querySelector("#sumar-form");
+// const div = document.querySelector("#resultado-div");
+
+function Matriz(filas, columnas) {
+  var matriz = [];
+  for (var i = 0; i < filas; i++) {
+    matriz[i] = [];
+    for (var j = 0; j < columnas; j++) {
+      
+      matriz[i][j] = "0"; 
+    }
+  }
+  return matriz;
+}
+var miMatriz=Matriz(3,4);
+console.log(miMatriz);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  // const firstNumber = Number.parseInt(first.value);
+  // const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  // div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+ 
 });
