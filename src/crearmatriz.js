3211fa24005d18976c21697bf3426355a6f5d845
Elian <eliantito234@gmail.com>
2023-04-04 00:58:11 -0400

    Ingresa en texto en tamaño de la matriz

--- src/crearmatriz.js
+++ src/crearmatriz.js
@@ -1,4 +1,7 @@
-function Matriz(filas, columnas) {
+function Matriz(limitecadena) {
+    const limites = obtenerNumeros(limitecadena);
+    const filas = parseInt(limites[0]); 
+    const columnas = parseInt(limites[1]);
     let matriz = new Array(filas);
     for (var i = 0; i < filas; i++) {
       matriz[i] = new Array(columnas);
@@ -7,6 +10,20 @@ function Matriz(filas, columnas) {
     return matriz;
   }
   
+  function obtenerNumeros(cadena) {
+    const numerosEnCadena = cadena.split(",");
+    const numeros = [];
+    for (let i = 0; i < numerosEnCadena.length; i++) {
+      const numero = parseInt(numerosEnCadena[i]);
+  
+      if (!isNaN(numero)) {
+        numeros.push(numero);
+      }
+    }
+    
+    return numeros;
+  }
+
  function valoresparaMatriz(matriz) {
     for(var fila = 0; fila < matriz.length; fila ++) {
       for(var columna = 0; columna < matriz[fila].length; columna ++) {
--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,6 @@
 import Matriz from "./crearmatriz";
 
-const f = document.querySelector("#primer-numero");
-const c = document.querySelector("#segundo-numero");
+const cadenalimite = document.querySelector("#cadena");
 const form = document.querySelector("#sumar-form");
 // const div = document.querySelector("#resultado-div");
 // Obtener la referencia al elemento div que contendrá la tabla
@@ -12,24 +11,9 @@ var tablaDiv = document.getElementById("tabla-matriz");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  let filas = Number.parseInt(f.value);
-  let columnas = Number.parseInt(c.value);
-  let miMatriz = Matriz(filas,columnas);
-  /*function Matriz(filas, columnas) {
-    var matriz = [];
-    for (var i = 0; i < filas; i++) {
-      matriz[i] = [];
-      for (var j = 0; j < columnas; j++) {
-        
-        matriz[i][j] = "0"; 
-      }
-    }
-    return matriz;
-  }*/
- 
+  
+  let miMatriz = Matriz(cadenalimite.value);
   var tabla = document.createElement("table");
-
   for (var i = 0; i < miMatriz.length; i++) {
   var fila = tabla.insertRow();
   for (var j = 0; j < miMatriz[i].length; j++) {
